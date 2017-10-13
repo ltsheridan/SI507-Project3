2ad7e7bb620c8782e531f3cd1ae3091b926757ad
ltsheridan <31678807+ltsheridan@users.noreply.github.com>
2017-10-12 20:58:41 -0400

    Part 1 complete

--- si507f17_project3_code.py
+++ si507f17_project3_code.py
@@ -69,9 +69,15 @@ state_elems=[]
 for elem in state_links:
     state_elems.append("https://www.nps.gov" + elem)
 
-# print (state_elems)
-# file_list=['arkansas_data.html','california_data.html','michigan_data.html']
+for elem in state_elems:
+    if "/mi/" in elem:
+        mich_url = elem
+    if "ar" in elem:
+        ark_url = elem
+    if "ca" in elem:
+        cal_url = elem
 
+print (mich_url)
 # TRY:
 # To open and read all 3 of the files
 # But if you can't, EXCEPT:
@@ -82,31 +88,34 @@ try:
     california_data = open("california_data.html",'r').read()
     michigan_data = open("michigan_data.html",'r').read()
 except:
-    soup = BeautifulSoup(nps_data,'html.parser')
-    ul = soup.find_all("ul"{"class":"dropdown-menu SearchBar-keywordSearch"})
-    for a in ul:
+    michigan_data = requests.get(mich_url).text
+    f = open("michigan_data.html", 'w')
+    f.write(michigan_data)
+    f.close()
+    california_data = requests.get(cal_url).text
+    f = open("california_data.html", 'w')
+    f.write(california_data)
+    f.close()
+    arkansas_data = requests.get(ark_url).text
+    f = open("arkansas_data.html",'w')
+    f.write(arkansas_data)
+    f.close()
 
-# Get a list of all the li (list elements) from the unordered list, using the BeautifulSoup find_all method
+
+
+# ul = soup.find('ul',{"class":"dropdown-menu SearchBar-keywordSearch"})
+# # Get a list of all the li (list elements) from the unordered list, using the BeautifulSoup find_all method
+# list_elems = ul.find_all("li")
+# # print (list_elems)
+
 # Use a list comprehension or accumulation to get all of the 'href' attributes of the 'a' tag objects in each li, instead of the full li objects
-
 # Filter the list of relative URLs you just got to include only the 3 you want: AR's, CA's, MI's, using the accumulator pattern & conditional statements
-
-
 # Create 3 URLs to access data from by appending those 3 href values to the main part of the NPS url. Save each URL in a variable.
-
-
 ## To figure out what URLs you want to get data from (as if you weren't told initially)...
 # As seen if you debug on the actual site. e.g. Maine parks URL is "http://www.nps.gov/state/me/index.htm", Michigan's is "http://www.nps.gov/state/mi/index.htm" -- so if you compare that to the values in those href attributes you just got... how can you build the full URLs?
-
-
 # Finally, get the HTML data from each of these URLs, and save it in the variables you used in the try clause
 # (Make sure they're the same variables you used in the try clause! Otherwise, all this code will run every time you run the program!)
-
-
 # And then, write each set of data to a file so this won't have to run again.
-
-
-
 
 
 
@@ -117,6 +126,10 @@ except:
 
 # - Create BeautifulSoup objects out of all the data you have access to in variables from Part 1
 # - Do some investigation on those BeautifulSoup objects. What data do you have about each state? How is it organized in HTML?
+
+mich_soup = BeautifulSoup(michigan_data,'html.parser')
+ark_soup = BeautifulSoup(arkansas_data, 'html.parser')
+cal_soup = BeautifulSoup(california_data, 'html.parser')
 
 # HINT: remember the method .prettify() on a BeautifulSoup object -- might be useful for your investigation! So, of course, might be .find or .find_all, etc...
 
@@ -129,21 +142,24 @@ except:
 # Remember that there are things you'll have to be careful about listed in the instructions -- e.g. if no type of park/site/monument is listed in input, one of your instance variables should have a None value...
 
 
-
-
-
 ## Define your class NationalSite here:
 
+class NationalSite(object):
+    def __init__(self):
+        self.location =
+        self.name =
+        self.park_type =
+        self.description =
 
 
 
 
 ## Recommendation: to test the class, at various points, uncomment the following code and invoke some of the methods / check out the instance variables of the test instance saved in the variable sample_inst:
 
-# f = open("sample_html_of_park.html",'r')
-# soup_park_inst = BeautifulSoup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
-# sample_inst = NationalSite(soup_park_inst)
-# f.close()
+f = open("sample_html_of_park.html",'r')
+soup_park_inst = BeautifulSoup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
+sample_inst = NationalSite(soup_park_inst)
+f.close()
 
 
 ######### PART 3 #########
